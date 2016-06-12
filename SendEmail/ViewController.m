200484305fe59474cdfce7c0db4633f04858fb8d
Tim <war011298@gmail.com>
2016-06-12 17:44:26 +0800

    Initial Commit

---
+++ SendEmail/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SendEmail
+//
+//  Created by Tim on 2016/6/12.
+//  Copyright © 2016年 Tim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SendEmail/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SendEmail
+//
+//  Created by Tim on 2016/6/12.
+//  Copyright © 2016年 Tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
